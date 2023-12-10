5cb02af7098cae3da21a0264739975924a7829ef
Vincent Palomo <vincentpalomo@yahoo.com>
2023-12-10 17:25:41 -0600

    styling for homepage completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,17 +5,24 @@ import logo from '../assets/logo centered.png';
 
 const home = () => {
   return (
-    <div className="bg-forest-green">
-      <div className="flex justify-center items-center h-48">
-        <img className="h-20" src={logo} alt="logo" />
-      </div>
-      <div className="flex justify-center">
-        <Navbar />
-      </div>
+    <div className="flex justify-center bg-forest-green">
+      <div className="bg-forest-green w-[1440px] h-screen relative">
+        <div className="flex justify-center items-center h-48">
+          <img className="h-20" src={logo} alt="logo" />
+        </div>
+        <div className="flex justify-center">
+          <Navbar />
+        </div>
 
-      <h1 className="font-playfair">abby morgan</h1>
-      <img src={profileImg} alt="abby morgan" />
-      <h3 className="font-playfair">software engineer</h3>
+        <div className="h-[450px] w-full flex justify-center">
+          <img className="h-[422px]" src={profileImg} alt="abby morgan" />
+        </div>
+
+        <h1 className="absolute font-playfair text-abby-pink text-[128px] bottom-[100px] left-10">abby</h1>
+        <h1 className="absolute font-playfair text-abby-pink text-[128px] bottom-0 left-10">morgan</h1>
+
+        <h3 className="absolute font-playfair text-off-white text-[64px] bottom-5 right-10">software engineer</h3>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
   return (
     <>
-      <div className="font-playfair text-3xl w-[422px] text-abby-pink flex justify-between ">
+      <div className="font-playfair text-[34px] w-[500px] text-abby-pink flex justify-between ">
         <div>home</div>
         <div>about</div>
         <div>projects</div>
