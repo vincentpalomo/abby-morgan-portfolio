9462c9f8a55fd51b8208eb7e92dd06944ab439c2
Vincent Palomo <vincentpalomo@yahoo.com>
2023-12-10 16:38:04 -0600

    installed tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Home } from './components';
+import './index.css';
 
 const App = () => {
   return (
     <>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
       <Home />
     </>
   );
