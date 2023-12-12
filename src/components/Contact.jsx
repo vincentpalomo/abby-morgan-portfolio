8de2586a07748418f2be1de9cb585b3bac110157
Vincent Palomo <vincentpalomo@yahoo.com>
2023-12-11 19:32:50 -0600

    implemented contact page with no functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navbar from './Navbar';
 import profileImg from '../assets/profile image placeholder.png';
+import github from '../assets/github.png';
+import email from '../assets/email.png';
+import linkedin from '../assets/linkedin.png';
 
 const Contact = () => {
   return (
@@ -11,11 +14,18 @@ const Contact = () => {
         <div className='flex flex-col bg-forest-green h-screen w-[1440px]'>
           <Navbar />
 
-          <div className='h-[450px] w-full flex justify-center'>
+          {/* <div className='h-[450px] w-full flex justify-center'>
             <img className='h-[422px]' src={profileImg} alt='abby morgan' />
-          </div>
+          </div> */}
 
-          <h1 className='font-playfair text-off-white text-9xl tracking-tight text-center'>under construction</h1>
+          <div className='h-1/2 flex flex-col items-center justify-center'>
+            <h1 className='font-playfair text-off-white text-9xl tracking-tight text-center'>let's talk</h1>
+            <div className='flex justify-evenly items-center h-44 w-1/3'>
+              <img className='h-[75px]' src={linkedin} alt='LinkedIn link' />
+              <img className='h-[75px]' src={email} alt='email link' />
+              <img className='h-[75px]' src={github} alt='github link' />
+            </div>
+          </div>
         </div>
       </div>
     </>
