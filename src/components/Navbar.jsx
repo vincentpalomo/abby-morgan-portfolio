f5a703380b7751cf1c91344e19fb74497df32185
Vincent Palomo <vincentpalomo@yahoo.com>
2023-12-14 18:36:47 -0600

    added comments for navbar mobile implementation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,27 +5,30 @@ import logo from '../assets/logo centered.png';
 const Navbar = () => {
   return (
     <>
-      <div className='flex flex-col justify-center items-center'>
-        <div className='flex justify-center items-center h-36'>
-          <Link to='/'>
-            <img className='h-[55px]' src={logo} alt='logo' />
+      {/* web view */}
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex justify-center items-center h-36">
+          <Link to="/">
+            <img className="h-[55px]" src={logo} alt="logo" />
           </Link>
         </div>
-        <div className='font-playfair text-[34px] w-[500px] text-abby-pink flex justify-between '>
-          <Link className='hover:text-off-white' to='/'>
+        <div className="font-playfair text-[34px] w-[500px] text-abby-pink flex justify-between ">
+          <Link className="hover:text-off-white" to="/">
             home
           </Link>
-          <Link className='hover:text-off-white' to='/about'>
+          <Link className="hover:text-off-white" to="/about">
             about
           </Link>
-          <Link className='hover:text-off-white' to='/projects'>
+          <Link className="hover:text-off-white" to="/projects">
             projects
           </Link>
-          <Link className='hover:text-off-white' to='/contact'>
+          <Link className="hover:text-off-white" to="/contact">
             contact
           </Link>
         </div>
       </div>
+
+      {/* mobile view */}
     </>
   );
 };
