dd14dbb4a5828cd216f9fdcac4dbf309899440bf
Vincent Palomo <vincentpalomo@yahoo.com>
2023-12-17 15:03:17 -0600

    removed unused code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,22 +12,22 @@ const Contact = () => {
     <>
       {/* web view */}
 
-      <div className='hidden sm:block'>
-        <div className='bg-forest-green flex justify-center'>
-          <div className='flex flex-col bg-forest-green h-screen w-[1440px]'>
+      <div className="hidden sm:block">
+        <div className="bg-forest-green flex justify-center">
+          <div className="flex flex-col bg-forest-green h-screen w-[1440px]">
             <Navbar />
 
-            <div className='h-1/2 flex flex-col items-center justify-center'>
-              <h1 className='font-playfair text-off-white text-9xl tracking-tight text-center'>let's talk</h1>
-              <div className='flex justify-evenly items-center h-44 w-1/3'>
-                <Link to=''>
-                  <img className='h-[75px]' src={linkedin} alt='LinkedIn link' />
+            <div className="h-1/2 flex flex-col items-center justify-center">
+              <h1 className="font-playfair text-off-white text-9xl tracking-tight text-center">let's talk</h1>
+              <div className="flex justify-evenly items-center h-44 w-1/3">
+                <Link to="">
+                  <img className="h-[75px]" src={linkedin} alt="LinkedIn link" />
                 </Link>
-                <Link to=''>
-                  <img className='h-[75px]' src={email} alt='email link' />
+                <Link to="">
+                  <img className="h-[75px]" src={email} alt="email link" />
                 </Link>
-                <Link to=''>
-                  <img className='h-[75px]' src={github} alt='github link' />
+                <Link to="">
+                  <img className="h-[75px]" src={github} alt="github link" />
                 </Link>
               </div>
             </div>
@@ -37,26 +37,22 @@ const Contact = () => {
 
       {/* mobile view */}
 
-      <div className='flex justify-center bg-forest-green sm:hidden'>
-        <div className='bg-forest-green w-full h-screen relative'>
-          {/* <div className="h-48 flex mx-10 justify-between items-center">
-            <img className=" h-16" src={logo} alt="logo" />
-            <img className=" h-7" src={hamburger} alt="mobile menu" />
-          </div> */}
+      <div className="flex justify-center bg-forest-green sm:hidden">
+        <div className="bg-forest-green w-full h-screen relative">
           <Navbar />
 
-          <div className='flex flex-col justify-center h-1/2'>
-            <h1 className='font-playfair text-center text-off-white text-[96px]'>let's talk</h1>
+          <div className="flex flex-col justify-center h-1/2">
+            <h1 className="font-playfair text-center text-off-white text-[96px]">let's talk</h1>
 
-            <div className='flex justify-evenly items-center h-32'>
-              <Link to=''>
-                <img className='h-[75px]' src={linkedin} alt='LinkedIn link' />
+            <div className="flex justify-evenly items-center h-32">
+              <Link to="">
+                <img className="h-[75px]" src={linkedin} alt="LinkedIn link" />
               </Link>
-              <Link to=''>
-                <img className='h-[75px]' src={email} alt='email link' />
+              <Link to="">
+                <img className="h-[75px]" src={email} alt="email link" />
               </Link>
-              <Link to=''>
-                <img className='h-[75px]' src={github} alt='github link' />
+              <Link to="">
+                <img className="h-[75px]" src={github} alt="github link" />
               </Link>
             </div>
           </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,18 +8,18 @@ const Projects = () => {
   return (
     <>
       {/* web view */}
-      <div className='hidden sm:block'>
-        <div className='flex justify-center bg-forest-green'>
-          <div className='bg-forest-green w-[1440px] h-screen'>
-            <div className='flex justify-center'>
+      <div className="hidden sm:block">
+        <div className="flex justify-center bg-forest-green">
+          <div className="bg-forest-green w-[1440px] h-screen">
+            <div className="flex justify-center">
               <Navbar />
             </div>
 
-            <div className='w-full flex flex-col items-center justify-center'>
-              <div className='h-[450px] w-full flex justify-center'>
-                <img className='h-[422px]' src={profileImg} alt='abby morgan' />
+            <div className="w-full flex flex-col items-center justify-center">
+              <div className="h-[450px] w-full flex justify-center">
+                <img className="h-[422px]" src={profileImg} alt="abby morgan" />
               </div>
-              <h1 className='font-playfair text-off-white text-9xl tracking-tight'>under construction</h1>
+              <h1 className="font-playfair text-off-white text-9xl tracking-tight">under construction</h1>
             </div>
           </div>
         </div>
@@ -27,15 +27,11 @@ const Projects = () => {
 
       {/* mobile view */}
 
-      <div className='flex justify-center bg-forest-green sm:hidden'>
-        <div className='bg-forest-green w-full h-screen relative'>
-          {/* <div className="h-48 flex mx-10 justify-between items-center">
-            <img className=" h-16" src={logo} alt="logo" />
-            <img className=" h-7" src={hamburger} alt="mobile menu" />
-          </div> */}
+      <div className="flex justify-center bg-forest-green sm:hidden">
+        <div className="bg-forest-green w-full h-screen relative">
           <Navbar />
 
-          <h1 className='font-playfair text-center text-off-white text-[96px]'>come back soon</h1>
+          <h1 className="font-playfair text-center text-off-white text-[96px]">come back soon</h1>
         </div>
       </div>
     </>
